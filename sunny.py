8eec360177322ad68aec63f9a2127fbc840a65ee
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-09 15:41:05 +0100

    Shortened code to cleaner and simpler code

--- sunny.py
+++ sunny.py
@@ -1,14 +1,13 @@
 # Asking the user whether it is sunny
-print("Is it sunny?")
+print("Is it currently sunny?")
 is_sunny = input()
 
-print ("Is there a breeze?")
+print ("Is there currently a breeze?")
 is_breeze = input().lower() # The .lower() part reads what the user typed and puts into lowercase
 
-if is_sunny == "yes":
-    if is_breeze == "yes":
-        print("Clothes are dry") # [R] if_sunny = yes, if_breeze = yes
-    else:
-        print("Clothes are drying slowly") # [R] if_sunny = yes, if_breeze = no
+if is_sunny == "yes" and is_breeze == "yes":
+        print("Your clothes are dry") # [R] if_sunny = yes, if_breeze = yes
+elif is_sunny == "yes" and is_breeze == "no":
+        print("Your clothes are drying slowly") # [R] if_sunny = yes, if_breeze = no
 else:
-    print("Clothes are not drying") # [R] if_sunny = no, if_breeze = no
+    print("Your clothes are not dry.") # [R] if_sunny = no, if_breeze = no
