a8e9a927c504dc769769020410dc9317025f8d43
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-09 16:25:05 +0100

    Added new file and added code

---
+++ float.py
@@ -0,0 +1,8 @@
+print("Please enter a number") # Asks for a number
+first_number = float(input())
+
+print("Please enter a second number") # Asks for a second number
+second_number = float(input())
+
+print("Please enter a third number") # Asks for a third number
+third_number = float(input())
