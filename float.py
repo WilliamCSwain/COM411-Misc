6f94d5699671b194fcd662219fe367ed97476325
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-09 16:33:35 +0100

    Amended code to include a while-loop.

--- float.py
+++ float.py
@@ -1,8 +1,7 @@
-print("Please enter a number") # Asks for a number
-first_number = float(input())
+count = 0
 
-print("Please enter a second number") # Asks for a second number
-second_number = float(input())
+while count < 5:
+    print("Please enter a number")
+    user_number = float(input())
 
-print("Please enter a third number") # Asks for a third number
-third_number = float(input())
+    count = count + 1
