ac52c1c56c33a43d67a68515d1edcd7387ce5522
WilliamCSwain <5SWAIW91@solent.ac.uk>
2021-10-09 16:45:59 +0100

    Amended code to print within a range

---
+++ for-loop.py
@@ -0,0 +1,11 @@
+for count in range(5):# Will generated a range of number up to 5, not including 5. 0,1,2,3,4
+    print("Please enter a number")
+    user_number = float(input())
+
+count = 0
+
+while count < 5:
+    print("Please enter a number")
+    user_number = float(input())
+
+    count = count + 1
